f3b8218bd59416352c58da259c777a32352b9b10
miguelOchoaM <miguel.ochoa@mercadolibre.com.co>
2023-12-15 17:28:47 -0500

    (Feat): Adding the get followers count endpoint and the exception handler for the not found exception

--- src/main/java/com/example/be_java_hisp_w23_g3/controller/UserController.java
+++ src/main/java/com/example/be_java_hisp_w23_g3/controller/UserController.java
@@ -1,9 +1,26 @@
 package com.example.be_java_hisp_w23_g3.controller;
 
+import com.example.be_java_hisp_w23_g3.service.user.UserService;
+import com.example.be_java_hisp_w23_g3.service.user.UserServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{userId}/followers/count")
+    public ResponseEntity<?> getFollowersCount(@PathVariable Long userId) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getFollowersCount(userId));
+    }
 }
