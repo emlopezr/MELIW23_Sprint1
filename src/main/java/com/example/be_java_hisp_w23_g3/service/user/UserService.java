aa2571fe366af67f96d0d7a4a474c44e2ae2836e
miguelOchoaM <miguel.ochoa@mercadolibre.com.co>
2023-12-15 17:29:43 -0500

    (Feat): Adding the get followers method in user service class

--- src/main/java/com/example/be_java_hisp_w23_g3/service/user/UserService.java
+++ src/main/java/com/example/be_java_hisp_w23_g3/service/user/UserService.java
@@ -1,4 +1,7 @@
 package com.example.be_java_hisp_w23_g3.service.user;
 
+import com.example.be_java_hisp_w23_g3.dto.response.FollowersCountDTO;
+
 public interface UserService {
+    FollowersCountDTO getFollowersCount(Long id);
 }
--- src/main/java/com/example/be_java_hisp_w23_g3/service/user/UserServiceImpl.java
+++ src/main/java/com/example/be_java_hisp_w23_g3/service/user/UserServiceImpl.java
@@ -1,7 +1,30 @@
 package com.example.be_java_hisp_w23_g3.service.user;
 
+import com.example.be_java_hisp_w23_g3.dto.response.FollowersCountDTO;
+import com.example.be_java_hisp_w23_g3.entity.Seller;
+import com.example.be_java_hisp_w23_g3.exception.NotFoundException;
+import com.example.be_java_hisp_w23_g3.repository.user.SellerRepository;
+import com.example.be_java_hisp_w23_g3.repository.user.SellerRepositoryImpl;
+import com.example.be_java_hisp_w23_g3.repository.user.UserRepository;
+import com.example.be_java_hisp_w23_g3.repository.user.UserRepositoryImpl;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final SellerRepository sellerRepository;
+
+    public UserServiceImpl(UserRepositoryImpl userRepository, SellerRepositoryImpl sellerRepository) {
+        this.userRepository = userRepository;
+        this.sellerRepository = sellerRepository;
+    }
+
+    @Override
+    public FollowersCountDTO getFollowersCount(Long userId) {
+        Seller seller = sellerRepository.findSellerById(userId);
+        if (seller == null)
+            throw new NotFoundException("Seller with id " + userId + " not found");
+        return new FollowersCountDTO(seller.getId(), seller.getUsername(), seller.getFollower().size());
+    }
 }
