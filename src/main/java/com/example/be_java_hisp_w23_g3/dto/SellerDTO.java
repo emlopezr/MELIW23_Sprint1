5d1ee2ed6fb0bec6823c8f16bcb0d39cf9ca7f8c
Sebastian Ospina Garcia <sebastian.ospina@mercadolibre.com.co>
2023-12-18 09:54:31 -0500

    (Feat): Added lists of Sellers to the Sellers Repository to make the already implemented code work.

--- src/main/java/com/example/be_java_hisp_w23_g3/dto/SellerDTO.java
+++ src/main/java/com/example/be_java_hisp_w23_g3/dto/SellerDTO.java
@@ -3,4 +3,7 @@ package com.example.be_java_hisp_w23_g3.dto;
 import com.example.be_java_hisp_w23_g3.dto.response.UserDTO;
 
 public class SellerDTO extends UserDTO {
+    public SellerDTO(Long userId, String userName) {
+        super(userId, userName);
+    }
 }
